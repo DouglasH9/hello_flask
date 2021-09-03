13b523456c9d76141938bc6e516eca506bac5f75
Doug Herman <douglash9882@gmail.com>
2021-09-02 23:19:59 -0500

    added color

--- playserver.py
+++ playserver.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/play/<int:times>")
+@app.route("/play")
 def displayBox():
-    return render_template('playground.html', 'times')
+    return render_template('playground.html', times=3, color= 'blue')
 
-# @app.route('/play/<int:times>')
-# def mult_boxes():
-#     return render_template('playground.html', times)
+@app.route('/play/<int:times>')
+def mult_boxes(times):
+    return render_template('playground.html', times=times, color= 'blue')
+
+@app.route('/play/<int:times>/<string:color>')
+def num_color_boxes(times, color):
+    return render_template('playground.html', times=times, color=color)
 
 if __name__=="__main__":
     app.run(debug=True)
